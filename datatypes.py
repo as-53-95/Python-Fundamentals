f118b702cac4ffa37d81f31aea9d32ca79f89482
as.53.95 <wafulaasher@gmail.com>
2026-02-11 12:38:28 +0300

    second commit

---
+++ datatypes.py
@@ -0,0 +1,31 @@
+age = 17 #integer
+temperature = 36.54 #float
+greeting = "hey" #string
+is_married = True #boolean
+
+print("I am",age,"years old")
+print("The current body temperature of our patient is",temperature,"degrees celcius")
+print(greeting,"Asher")
+print("Are you married?",is_married)
+
+#A datastructure refers to multiple values stored in a single variable
+#they include, lists, tuples, dictionaries and arrays
+
+cars = ["Toyota","Mercedes", "BMW"] #list-Ordered and changeable
+fruits =("Banana","Mango","Grape","Avocado") #tuple-ordered and unchangeable
+countries = {"Italy","France","Kenya","Taiwan"} #set-unordered and unchangeable
+Student = {
+    "name" : "Asher Wafula",
+    "age" : 17,
+    "gender" : "Male",
+    "course" : "FullStack"
+}
+
+print(cars)
+print(fruits)
+print(countries)
+print(Student)
+print(Student["name"])
+
+#Typecasting
+
---
+++ variants.py
@@ -0,0 +1,15 @@
+#variables are containers that hold the values they are assigned
+
+firstname = "Asher" #str
+school = "E-Mobilis"
+lastname = "Wafula"
+num1 = 56 #int
+num2 = 20
+language = "python"
+
+print(firstname) #do not close variables in double quotes or else python will think it is a text
+print(school)
+print(firstname,lastname)
+print(school,"is a coding school")
+print(num1 + num2)
+print(language,"is a web programming language")
